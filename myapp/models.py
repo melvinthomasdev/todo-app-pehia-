f44245c0fd75e41f9e205b5c839f7d5895bb3f60
Melvin Thomas <melvinchooranolil@gmail.com>
2021-05-11 18:48:12 +0530

    models created

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+'''CREATE TABLE <table_name> (
+id integer(100) primary key,
+title varchar(50) not null,
+description varchar(150) not null
+);'''
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50, null=False)
+    description = models.CharField(max_length=150, null=False)

5a0a2d9c2bfc1dea90bd1225f34f21a430e5d83e
Melvin Thomas <melvinchooranolil@gmail.com>
2021-05-14 16:11:02 +0530

    views done

--- myapp/models.py
+++ myapp/models.py
@@ -11,3 +11,6 @@ description varchar(150) not null
 class Todo(models.Model):
     title = models.CharField(max_length=50, null=False)
     description = models.CharField(max_length=150, null=False)
+
+    def __str__(self):
+        return self.title
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
+from .models import Todo
 # Create your views here.
+
+# def homepageview(request):
+#     return HttpResponse("""<!DOCTYPE html>
+# <html lang="en">
+# <head>
+#     <meta charset="UTF-8">
+#     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+#     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+#     <title>Hello Django</title>
+# </head>
+# <body>
+#     <h1> This is a Heading</h1>
+#     <h2> This is another heading...</h2>
+# </body>
+# </html>""")
+
+
+def homepageview(request):
+    context = {}
+    todos = Todo.objects.all()
+    context['works'] = todos
+    context['message'] = "Hello Sanjana, this is a message for you..."
+    return render(request, 'index.html', context)
